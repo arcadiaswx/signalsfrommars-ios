c819ff68593a655d7086a695fb3c0208bbbca5a6
Craig Booker <info@itech.plus>
2018-07-09 09:39:02 -0500

    Added Story data model class.

---
+++ FunFacts/Story.swift
@@ -0,0 +1,21 @@
+//
+//  Story.swift
+//  FunFacts
+//
+//  Created by Craig on 7/9/18.
+//  Copyright © 2018 ITECH PLUS. All rights reserved.
+//
+
+import UIKit
+
+enum Story {
+    case returnTrip
+    case touchDown
+    case homeward
+    case rover
+    case cave
+    case crate
+    case monster
+    case droid
+    case home
+}
